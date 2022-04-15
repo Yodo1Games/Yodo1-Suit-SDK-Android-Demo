bc19882e867018ee2314cf4b8ebb61bb3e03ec15
HunterZhang <zhangjianqiu007@126.com>
2022-04-15 18:06:01 +0800

    add AF report button.

--- app/src/main/java/com/yodo1/demo/FragmentAnalytics.java
+++ app/src/main/java/com/yodo1/demo/FragmentAnalytics.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import com.yodo1.android.sdk.open.Yodo1Analytics;
+
 
 /**
  *
@@ -39,13 +41,40 @@ public class FragmentAnalytics extends Fragment implements View.OnClickListener 
         body = inflater.inflate(R.layout.fragment_analytics, container, false);
         editText = body.findViewById(R.id.et_activation_code);
         body.findViewById(R.id.eventname).setOnClickListener(this);
+        body.findViewById(R.id.appsflyer).setOnClickListener(this);
         return body;
     }
 
     @Override
     public void onClick(View v) {
+        String ets = editText.getText().toString();
         switch (v.getId()) {
             case R.id.eventname:
+                Yodo1Analytics.onCustomEvent(mContext,ets);
+//                Yodo1Analytics.onCustomEvent(ets,jsonData);
+//
+//                Yodo1Analytics.onChargeFail(orderId);
+//                Yodo1Analytics.onChargeRequest(order,itemid,currencyAmount,crrencyType,virualCurrencyAmount,paymentType);
+//                Yodo1Analytics.onChargeSuccess(orderId);
+//                Yodo1Analytics.onPurchase(itemId,buyNum,gamecoin);
+//                Yodo1Analytics.onValidateInAppPurchase(publicKey,siganature,purchaseData,price,currency);
+//
+//                Yodo1Analytics.onMissionBegion(missionId);
+//                Yodo1Analytics.onMissionCompleted(missionid);
+//                Yodo1Analytics.onMissionFailed(missionid,cause);
+//                Yodo1Analytics.onReward(gamecoin,trigger,reason);
+//                Yodo1Analytics.onUseItem(itemid,useNum,gamecoin);
+//
+//                Yodo1Analytics.login(account);
+//                Yodo1Analytics.logout();
+//                Yodo1Analytics.setAccount(account);
+                break;
+            case R.id.appsflyer:
+                Yodo1Analytics.onCustomEventAppsflyer("test",null);
+
+//                Yodo1Analytics.setDoNotSell(false);
+//                Yodo1Analytics.setAgeRestrictedUser(false);
+//                Yodo1Analytics.setHasUserConsent(false);
                 break;
             default:
         }
